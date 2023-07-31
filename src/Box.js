55be96a36e48d309270fab7c3d0ea0634c8640aa
jvill171 <juanvillarreal64@yahoo.com>
2023-07-31 02:15:56 -0500

    Added default values for height/width

--- src/Box.js
+++ src/Box.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-const Box = ({id, backgroundColor, width, height, handleRemove}) => {
+const Box = ({id, backgroundColor, width=5, height=5, handleRemove}) => {
 
     return (
         <div
-        id={id}
-        style={{backgroundColor, width, height, border:"1px solid black", margin:"5px"}}>
+            className="box"
+            id={id}
+            style={{
+                backgroundColor,
+                width:`${width}em`,
+                height:`${height}em`,
+                border:"1px solid black", margin:"5px"
+            }}
+        >
             <button onClick={handleRemove}>X</button>
         </div>
     )
